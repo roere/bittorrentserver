cd7a7b4f82cde57f3bdaf4e048b961c0ef9f3d33
roere <roere76@gmail.com>
2017-06-02 22:00:05 +0200

    Update bittorrentserver.py

--- bittorrentserver.py
+++ bittorrentserver.py
@@ -14,6 +14,8 @@ torrentHandleList = [] #Storage for Torrent handles
 torrentList = [] #Storage for Torrents
 appName = "bittorrentserver"
 basePath = "./addon/"+appName+"/"
+cloudFileBasePath = "./"
+
 #basePath = "./"
 configFileName = appName+".cfg"
 magnetURIFileName = "magnetURI.txt"
@@ -24,7 +26,7 @@ trackerList = []
 def inputController(ses,):
     global run
     while run:
-        logFile.write("Input Controller ...\n")
+        logFile.write("Input Controller started...\n")
         logFile.flush()
         inputString = input()
             
@@ -83,89 +85,136 @@ def inputController(ses,):
 #outputController: write status message into pingFile that can be read by the plugin
 def outputController (ses,):
     global run
-    while run:
-        with open(os.path.normpath(basePath+appName+".ping"),"w") as pingFile:
-            pingFile.write('['+time.strftime("%d.%m.%Y - %H:%M:%S")+'] Process ID: '+str(os.getpid())+' Listening on: %d' % \
-                           (ses.listen_port()))
-            for handle in torrentHandleList:
-                s = handle.status()
-                state_str = ['queued', 'checking', 'downloading metadata', \
-                             'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
-                pingFile.write('\n%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
-                                (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
-        pingFile.close()
-        time.sleep(10)   
-    logFile.write("SIGTERM: outputController ended...\n")
-    logFile.flush()
+    try:
+        logFile.write("OutputController started...\n")
+        logFile.flush()
+        while run:
+            with open(os.path.normpath(basePath+appName+".ping"),"w") as pingFile:
+                pingFile.write('['+time.strftime("%d.%m.%Y - %H:%M:%S")+'] Process ID: '+str(os.getpid())+' Listening on: %d' % \
+                               (ses.listen_port()))
+                for handle in torrentHandleList:
+                    s = handle.status()
+                    state_str = ['queued', 'checking', 'downloading metadata', \
+                                 'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+                    pingFile.write('\n%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+                                    (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
+            pingFile.close()
+            time.sleep(10)   
+        logFile.write("SIGTERM: outputController ended...\n")
+        logFile.flush()
+    except:
+        logFile.write("Unexpected error:\n->"+str(sys.exc_info()[0])+"\n->"+str(sys.exc_info()[1])+"\n->"+str(sys.exc_info()[2])+"\n")
+        logFile.flush()
+        run = False
     
 #configFileController: check config File for changes and react on them    
 def configFileController (ses,):
     global run, appName, basePath
-    stamp = ""
-    config = configparser.ConfigParser()
-    while run:
-        if stamp != os.stat(basePath+configFileName).st_mtime:
-            stamp = os.stat(basePath+configFileName).st_mtime
-            config.read(os.path.normpath(basePath+configFileName))
-            if config["Controller"]["sigterm"] == "1":
-                run = False  
-                logFile.write("SIGTERM recieved...\n")
-                logFile.flush()
-            elif config["Controller"]["sigreload"] == "1":
-                logFile.write("SIGRELOAD: reloading config file...\n")
-                logFile.flush()
-                #add Trackers
-                trackerList = config.items("Tracker")
-                lList = []
-                for tracker in trackerList:
-                    lList.append(tracker[1].replace("\"",""))
-                appendTrackerList(lList)
-                
-                #add Files
-                with open(os.path.normpath(basePath+magnetURIFileName), 'w') as magnetURIOutFile:
-                    fileList = config.items("File")
+    stamp = "" #time stamp of cfg-file
+    try:
+        logFile.write("ConfigFileController started...\n")
+        logFile.flush()
+        config = configparser.ConfigParser()
+        while run:
+            if stamp != os.stat(basePath+configFileName).st_mtime:
+                stamp = os.stat(basePath+configFileName).st_mtime
+                config.read(os.path.normpath(basePath+configFileName))
+                if config["Controller"]["sigterm"] == "1":
+                    run = False  
+                    logFile.write("SIGTERM recieved...\n")
+                    logFile.flush()
+                elif config["Controller"]["sigreload"] == "1":
+                    logFile.write("SIGRELOAD: reloading config file...\n")
+                    logFile.flush()
+                    #add Trackers
+                    trackerList = config.items("Tracker")
+                    lList = []
+                    for tracker in trackerList:
+                        lList.append(tracker[1].replace("\"",""))
+                    appendTrackerList(lList)
                     
-                    #check all files
-                    j=1
-                    for mFile in fileList:
-                       
-                        fRaw = mFile[1].replace("\"","") #delete " in file name
-                        i = fRaw.rfind("/")
-                        if i==-1: #just file name given? we expect th e file relative to basePath 
-                            fPath = basePath
-                            fName = fRaw
-                        else:
-                            fName = fRaw[i+1:] #extract file name from given path
-                            if fRaw[0]=="/": #absolute path defined?
-                                fPath = fRaw[:i+1]
+                    #add Files
+                    with open(os.path.normpath(basePath+magnetURIFileName), 'w') as magnetURIOutFile:
+                        fileList = config.items("File")
+                        
+                        #check all files
+                        j=1
+                        for mFile in fileList:
+                           
+                            fRaw = mFile[1].replace("\"","") #delete " in file name
+                            i = fRaw.rfind("/")
+                            if i==-1: #just file name given? we expect the file relative to basePath 
+                                fPath = basePath
+                                fName = fRaw
                             else:
-                                fPath = basePath+fRaw[:i+1] #relative path? add basePath!
-    
-                        logFile.write("SIGRELOAD: reloading:"+fPath+"---"+fName+"\n")
-                        logFile.flush()
-                        if os.path.isfile(fPath+fName):
-                            mLink = addTorrent(fPath, fName)
-                            logFile.write("SIGRELOAD: Magnet-Link:"+mLink+"\n")
+                                fName = fRaw[i+1:] #extract file name from given path
+                                if fRaw[0]=="/": #absolute path defined?
+                                    fPath = fRaw[:i+1]
+                                else:
+                                    fPath = basePath+fRaw[:i+1] #relative path? add basePath!
+        
+                            logFile.write("SIGRELOAD: reloading:"+fPath+"---"+fName+"\n")
                             logFile.flush()
-                            magnetURIOutFile.write("["+str(j)+"]"+mLink+"\n")
+                            if os.path.isfile(fPath+fName):
+                                mLink = addTorrent(fPath, fPath, fName, fName)
+                                logFile.write("SIGRELOAD: Magnet-Link:"+mLink+"\n")
+                                logFile.flush()
+                                magnetURIOutFile.write("["+str(j)+"] "+mLink+"\n")
+                            else:
+                                logFile.write("SIGRELOAD: file not found:"+os.path.normpath(fPath+fName)+"\n")
+                                logFile.flush() 
+                                magnetURIOutFile.write("["+str(j)+"] File not found:"+os.path.normpath(fPath+fName)+"\n")
+                            j=j+1       
+                        
+                        #Cloud Files start
+                        if config.has_section ("Cloudfile"):
+                            cloudFileList = config.items("Cloudfile")
+                            logFile.write("SIGRELOAD: has section Cloudfile\n")
+                            logFile.flush()
+                            #check all files
+                            #j=1
+                            for cFile in cloudFileList:
+                               
+                                fRaw = cFile[0]
+                                i = fRaw.rfind("/")
+                                osfName = fRaw[i+1:] #extract hashed file name   
+                                fPath = cloudFileBasePath+fRaw[:i+1]
+                                fName = cFile[1] #real file name
+                                
+                                logFile.write("SIGRELOAD: reloading:"+fPath+"--- ("+fName+")\n")
+                                logFile.flush()
+                                if os.path.isfile(fPath+osfName):
+                                    mLink = addTorrent(fPath, (basePath+"torrents/"), osfName, fName)
+                                    logFile.write("SIGRELOAD: Magnet-Link:"+mLink+"\n")
+                                    logFile.flush()
+                                    magnetURIOutFile.write("["+str(j)+"] "+mLink+"\n")
+                                else:
+                                    logFile.write("SIGRELOAD: file not found:"+os.path.normpath(fPath+osfName)+" ("+fName+")\n")
+                                    logFile.flush() 
+                                    magnetURIOutFile.write("["+str(j)+"] File not found:"+os.path.normpath(fPath+osfName)+" ("+fName+"\n")
+                                j=j+1    
                         else:
-                            logFile.write("SIGRELOAD: file not found:"+os.path.normpath(fPath+fName)+"\n")
-                            logFile.flush() 
-                            magnetURIOutFile.write("["+str(j)+"] File not found:"+os.path.normpath(fPath+fName)+"\n")
-                        j=j+1       
-                magnetURIOutFile.close()
-                config["Controller"]["sigreload"] = "0"
-                logFile.write("SIGRELOAD: resetting SIGRELOAD...")
-                logFile.flush()
-                with open(os.path.normpath(basePath+configFileName), 'w') as configfile:
-                    config.write(configfile)
-                logFile.write("SIGRELOAD: reset!...")
-                logFile.flush()
-                    
-        else:
-            time.sleep(1)
-    logFile.write("SIGTERM: configFileController ended...\n")
-    logFile.flush()
+                            logFile.write("SIGRELOAD: has NO section Cloudfile")
+                            logFile.flush()
+                            #Cloud Files end
+                           
+                    magnetURIOutFile.close()
+                    config["Controller"]["sigreload"] = "0"
+                    logFile.write("SIGRELOAD: resetting SIGRELOAD...\n")
+                    logFile.flush()
+                    with open(os.path.normpath(basePath+configFileName), 'w') as configfile:
+                        config.write(configfile)
+                    logFile.write("SIGRELOAD: reset!...\n")
+                    logFile.flush()
+                        
+            else:
+                time.sleep(1)
+        logFile.write("SIGTERM: configFileController ended...\n")
+        logFile.flush()
+    except:
+        logFile.write("Unexpected error in ConfigFileController:\n-> "+str(sys.exc_info()[0])+"\n-> "+str(sys.exc_info()[1])+"\n-> "+str(sys.exc_info()[2])+"\n")
+        logFile.flush()
+        run = False
     
 #Add Bittorrent-Tracker
 def addTracker (tracker):
@@ -193,25 +242,26 @@ def addTrackerList (torrent):
         torrent.add_tracker(tracker)
             
 #
-def addTorrent (filepath, filename):
+def addTorrent (filepath, torrentpath, osfilename, filename):
     global torrentList, torrentHandleList, ses
     fileStorage = lt.file_storage()
-    lt.add_files(fileStorage, os.path.normpath(filepath+filename))
+    lt.add_files(fileStorage, os.path.normpath(filepath+osfilename))
     lTorrent = lt.create_torrent(fileStorage) #lTorrent = localTorrent
     torrentList.append(lTorrent)
     addTrackerList(lTorrent)
     
     lTorrent.set_creator('Hubzilla using Libtorrent %s' % lt.version)
     lTorrent.set_comment("Filename:"+filename)
+    
     lt.set_piece_hashes(lTorrent, os.path.normpath(filepath))
     gTorrent = lTorrent.generate() #gTorrent = generated Torrent
     
     ##write Torrent file to filesystem    
-    tFile = open(os.path.normpath(filepath+filename+".torrent"), "wb")
+    tFile = open(os.path.normpath(torrentpath+filename+".torrent"), "wb")
     tFile.write(lt.bencode(gTorrent))
     tFile.close()
     
-    handle = ses.add_torrent({'ti': lt.torrent_info(os.path.normpath(filepath+filename+".torrent")), 'save_path': os.path.normpath(filepath), 'seed_mode': True})
+    handle = ses.add_torrent({'ti': lt.torrent_info(os.path.normpath(torrentpath+filename+".torrent")), 'save_path': os.path.normpath(filepath), 'seed_mode': True})
     torrentHandleList.append(handle)
     
     mLink = lt.make_magnet_uri(lt.torrent_info(gTorrent))
